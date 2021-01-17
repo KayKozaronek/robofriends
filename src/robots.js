38556b113df87eb8132ed9da9246c172a1bd0144
KayKozaronek <kozaronek@gmail.com>
2021-01-17 19:11:26 +0100

    Adding the React - Robofriend project

---
+++ src/robots.js
@@ -0,0 +1,51 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Fabian Sdzuy",
+        username: "Sdzuyzid",
+        email: "asdfasd@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Patrick Jegers",
+        username: "P-Dog",
+        email: "asdfasd@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Michael Krause",
+        username: "Michy",
+        email: "asdfasd@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Alex Krause",
+        username: "Officer Krause",
+        email: "asdfasd@gmail.com"
+    },
+    {
+        id: 5,
+        name: "Adrian Roloff",
+        username: "Statist",
+        email: "asdfasd@gmail.com"
+    },
+    {
+        id: 6,
+        name: "Julian Ruwe",
+        username: "Raver",
+        email: "asdfasd@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Pascal Klingberger",
+        username: "Big P",
+        email: "asdfasd@gmail.com"
+    },
+    {
+        id: 8,
+        name: "Kay Kozaronek",
+        username: "Koschi",
+        email: "asdfasd@gmail.com"
+    },
+    
+]
